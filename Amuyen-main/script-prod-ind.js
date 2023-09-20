1c5f28a72a68dc99f454b3fead2d1b08c4ca51ab
fsilva95 <126418666+fsilva95@users.noreply.github.com>
2023-09-20 18:27:41 -0300

    Arreglo atributo imagen script-prod-ind.js
    Signed-off-by: fsilva95 <126418666+fsilva95@users.noreply.github.com>

--- Amuyen-main/script-prod-ind.js
+++ Amuyen-main/script-prod-ind.js
@@ -8,73 +8,73 @@ document.addEventListener("DOMContentLoaded", function () {
             nombre: "Almendras Enteras x 1 kg",
             precio: "$7.000,00",
             descripcion: "Deliciosas almendras enteras para disfrutar como snack o agregar a tus recetas.",
-            imagen: "/images/products/almendras.jpg"
+            imagen: "images/products/almendras.jpg"
         },
         "nueces": {
             nombre: "Nuez Mariposa Blanca x 1 kg",
             precio: "$5.500,00",
             descripcion: "Nueces de alta calidad, perfectas para aperitivos y postres.",
-            imagen: "/images/products/nueces.jpg"
+            imagen: "images/products/nueces.jpg"
         },
         "pasas-uva": {
             nombre: "Pasas de Uva x 1 kg",
             precio: "$1.500,00",
             descripcion: "Deliciosas pasas de uva para disfrutar solas o en tus recetas favoritas.",
-            imagen: "/images/products/pasas.jpg"
+            imagen: "images/products/pasas.jpg"
         },
         "mani-tostado": {
             nombre: "Mani Tostado x 1 kg",
             precio: "$1.500,00",
             descripcion: "Maní tostado y salado, ideal como snack o complemento en ensaladas.",
-            imagen: "/images/products/mani-tostado.jpg",
+            imagen: "images/products/mani-tostado.jpg",
         },
         "coco-rallado": {
             nombre: "Coco rallado High Fat x 1 kg",
             precio: "$4.300,00",
             descripcion: "Coco rallado de alta calidad, perfecto para repostería y postres.",
-            imagen: "/images/products/coco-rallado.jpg",
+            imagen: "images/products/coco-rallado.jpg",
         },
         "mix-amuyen": {
             nombre: "Mix Amuyén",
             precio: "$3.500,00",
             descripcion: "Una opción saludable para picar: maní tostado, almendras, nueces, maní japonés, maíz tostado, banana deshidratada.",
-            imagen: "/images/products/mix1.jpg",
+            imagen: "images/products/mix1.jpg",
         },
         "almohaditas": {
             nombre: "Almohaditas rellenas sabor Chocolate x 1 kg",
             precio: "$3.200,00",
             descripcion: "Almohaditas rellenas de chocolate, el snack perfecto para los amantes del chocolate.",
-            imagen: "/images/products/almohaditas.jpg",
+            imagen: "images/products/almohaditas.jpg",
         },
         "castanas-caju": {
             nombre: "Castañas de Cajú x 1 kg",
             precio: "$15.200,00",
             descripcion: "Castañas de cajú de alta calidad, ideales para aperitivos y platos gourmet.",
-            imagen: "/images/products/castanas.jpg",
+            imagen: "images/products/castanas.jpg",
         },
         "cereales-frutales": {
             nombre: "Cereales frutales x 1 kg",
             precio: "$2.000,00",
             descripcion: "Mezcla de cereales y frutas deshidratadas para un desayuno saludable.",
-            imagen: "/images/products/cereales-fruta.jpg",
+            imagen: "images/products/cereales-fruta.jpg",
         },
         "azucar-mascabo": {
             nombre: "Azucar Mascabo x 1 kg",
             precio: "$1.800,00",
             descripcion: "Azúcar mascabo natural, perfecta para endulzar tus bebidas y postres.",
-            imagen: "/images/products/azucar-mascabo.jpg",
+            imagen: "images/products/azucar-mascabo.jpg",
         },
         "rebozador": {
             nombre: "Rebozador para horneados y frituras libre de gluten x 500 g",
             precio: "$1.100,00",
             descripcion: "Rebozador sin gluten para preparar deliciosos platos horneados y fritos.",
-            imagen: "/images/products/premezclas.jpg",
+            imagen: "images/products/premezclas.jpg",
         },
         "premezclas": {
             nombre: "Premezcla universal para panadería y repostería libre de gluten x 500 g",
             precio: "$1.100,00",
             descripcion: "Premezcla sin gluten ideal para la preparación de panes y repostería.",
-            imagen: "/images/products/premezclas.jpg",
+            imagen: "images/products/premezclas.jpg",
         }
     };
 
